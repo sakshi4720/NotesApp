3b4f5643f34e994bec004aefa83220b9a12de032
Arpan Sharma <arpansharma@zapbuild.com>
2021-08-18 12:40:25 +0530

    Fixes - minor

--- src/services/RootNavigator/index.tsx
+++ src/services/RootNavigator/index.tsx
@@ -7,10 +7,10 @@ import SplashScreen from '../../container/SplashScreen';
 
 
 export type RootStackParamList = {
-    SplashScreen:undefined,
+    SplashScreen: undefined,
     Home: undefined,
     EnterNotes: undefined;
-    DetailedNotes: {userNotes: string | undefined} ;
+    DetailedNotes: { userNotes: string | undefined };
 };
 
 const RootStack = createStackNavigator<RootStackParamList>();
@@ -23,7 +23,7 @@ const RootNavigator: React.FC<Props> = () => {
     const { Navigator, Screen } = RootStack
     return (
         <Navigator initialRouteName="SplashScreen">
-              <Screen name="SplashScreen" component={SplashScreen} />
+            <Screen name="SplashScreen" component={SplashScreen} />
             <Screen name="Home" component={Home} />
             <Screen name="EnterNotes" component={EnterNotes} />
             <Screen name="DetailedNotes" component={DetailedNotes} />
